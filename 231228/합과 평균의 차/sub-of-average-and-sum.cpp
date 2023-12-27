8324139e077d002fc6353497d33171c87b662774
Habin <108309664+HABINOH@users.noreply.github.com>
2023-12-28 00:27:15 +0900

    Title: 합과 평균의 차, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231228/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << a+b+c << "\n" << (a+b+c)/3 << "\n" << (a+b+c) - (a+b+c)/3;
+    
+    return 0;
+}
