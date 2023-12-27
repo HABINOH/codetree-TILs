eed047fda0d510f26da07ffa57fca0656d7b5aa2
Habin <108309664+HABINOH@users.noreply.github.com>
2023-12-28 00:19:49 +0900

    Title: 합과 평균, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231228/합과 평균/sum-and-avg.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String number = br.readLine();
+        String[] numbers = number.split(" ");
+
+        int a = Integer.parseInt(numbers[0]);
+        int b = Integer.parseInt(numbers[1]);
+
+        System.out.println(a+b + " " + (a+b)/2.0);
+    }
+}
