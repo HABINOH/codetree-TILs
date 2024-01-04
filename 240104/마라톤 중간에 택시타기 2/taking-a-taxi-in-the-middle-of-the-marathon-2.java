62bd5402ea7e3b323f66a90c7daf56ad6b13ac71
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-04 15:24:29 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

--- 240104/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
+++ 240104/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.java
@@ -3,8 +3,30 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args)throws IOException {
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-       
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer stk;
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for(int i=0;i<n;i++){
+            stk = new StringTokenizer(br.readLine(), " ");
+            
+            x[i] = Integer.parseInt(stk.nextToken());
+            y[i] = Integer.parseInt(stk.nextToken());
+        }
+        int maxNumber = Integer.MAX_VALUE;
+        for(int i=1;i<n-1;i++){
+            int dist = 0;
+            int preIndex = 0;
+            for(int j=1;j<n;j++){
+                if(i==j){continue;}
+                dist += Math.abs(x[preIndex] - x[j]) + Math.abs(y[preIndex] - y[j]);
+                preIndex = j;
+            }
+            maxNumber = Math.min(maxNumber, dist);
+        }
+        System.out.println(maxNumber);
     }
 }
