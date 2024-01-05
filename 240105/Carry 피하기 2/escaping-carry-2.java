3278934c78671fb2e8f9f1c13a97d45d04836b5c
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-05 13:50:18 +0900

    Title: Carry 피하기 2, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240105/Carry 피하기 2/escaping-carry-2.java
+++ 240105/Carry 피하기 2/escaping-carry-2.java
@@ -11,7 +11,7 @@ public class Main {
         for(int i=0;i<n;i++){
             numbers[i] = br.readLine();
         }
-        int result = 0;
+        int result = -1;
         for(int i=0; i<n-2; i++){
             for(int j=i+1; j<n-1; j++){
                 for(int k=j+1; k<n; k++){
