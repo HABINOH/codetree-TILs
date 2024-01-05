7fb5a064a3d00cb727c5bcaa78e22d473c38982a
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-05 15:36:42 +0900

    Title: 오목, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

--- 240105/오목/O-mok.java
+++ 240105/오목/O-mok.java
@@ -2,8 +2,8 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static int[] dx = new int[]{0,1,0,-1};
-    public static int[] dy = new int[]{1,0,-1,0};
+    public static int[] dx = new int[]{1, 1, 1, -1, -1, -1, 0, 0};
+    public static int[] dy = new int[]{-1, 0, 1, -1, 0, 1, -1, 1};
     public static int[][] map = new int[19][19];
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,37 +11,39 @@ public class Main {
             StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
             int j=0;
             while(stk.hasMoreTokens()){
-                map[i][j] = Integer.parseInt(stk.nextToken());
+                map[i][j++] = Integer.parseInt(stk.nextToken());
             }
         }
         int result = 0;
         for(int i=0;i<19;i++){
             for(int j=0;j<19;j++){
                 if(map[i][j] == 1 || map[i][j] == 2){
-                    if(dfs(i,j, 1)){
-                        result = map[i][j];
+                    for(int k=0;k<8;k++){
+                        int cnt = 1;
+                        int curX = i;
+                        int curY = j;
+
+                        while(true){
+                            int nx = curX + dx[k];
+                            int ny = curY + dy[k];
+                            if(inRange(nx,ny)){break;}
+                            if(map[nx][ny] != map[curX][curY]){break;}
+                            cnt++;
+                            curX = nx; curY = ny;
+                        }
+                        if(cnt == 5){
+                            System.out.println(map[i][j]);
+                            System.out.println((i + 2 * dx[k] + 1) + " " + (j + 2 * dy[k] + 1));
+                            System.exit(0);
+                        }
                     }
                 }
             }
         }
-        System.out.println(result);
+        System.out.println(0);
     }
-    public static boolean dfs(int x, int y, int cnt){
-        if(cnt == 5){
-            return true;
-        }
-        for(int i=0;i<4;i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(Inrange(nx,ny)){continue;}
-            if(map[nx][ny] == map[x][y]){
-                dfs(nx,ny,cnt+1);
-            }
-        }
-        return false;
-    }
-    public static boolean Inrange(int x, int y){
+    
+    public static boolean inRange(int x, int y){
         return x<0 || y<0 || x>=19 || y>=19;
     }
 }
