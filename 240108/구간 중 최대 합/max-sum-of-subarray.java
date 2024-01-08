500ac26d1143fcfc1b1310a0c7dee4f4e0706cf2
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-08 16:58:16 +0900

    Title: 구간 중 최대 합, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240108/구간 중 최대 합/max-sum-of-subarray.java
+++ 240108/구간 중 최대 합/max-sum-of-subarray.java
@@ -18,7 +18,7 @@ public class Main {
         int result = 0;
         for(int i=0;i<n-k;i++){
             int sum = 0;
-            for(int j=i;j<i+k;j++){
+            for(int j=i;j<=i+k;j++){
                 sum += numbers[j];
             }
             result = Math.max(result, sum);
