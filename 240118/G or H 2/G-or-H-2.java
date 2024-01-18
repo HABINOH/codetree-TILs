6294144fbed6649ca631bb5afcd65a101952a710
Habin <108309664+HABINOH@users.noreply.github.com>
2024-01-18 22:59:54 +0900

    Title: G or H 2, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

--- 240118/G or H 2/G-or-H-2.java
+++ 240118/G or H 2/G-or-H-2.java
@@ -1,53 +1,35 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static char[] line = new char[101]; 
+    public static char[] line = new char[102]; 
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int start = 201;
-        int end = 0;
         for(int i=0;i<n;i++){
             StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
             int pos = Integer.parseInt(stk.nextToken());
             char ch = stk.nextToken().charAt(0);
             line[pos] = ch;
-            start = Math.min(start, pos);
-            end = Math.max(end, pos);
         }
         //start 와 end 를 잡고
         //H와 G 개수가 다르면 start를 한칸 땡기고
         //비교하고 그래도 다르면 end를 한칸 땡기고
-        //반복
+        //반복 -> 2중 for문으로 완전탐색
         int answer = 0;
-        boolean flag = true;
-        while(start!=end){
-            int cntH = 0;
-            int cntG = 0;
-            for(int i=start; i<=end; i++){
-                if(line[i] == 'H'){cntH++;}
-                else if(line[i] == 'G'){cntG++;}
-            }
-            if(cntH == cntG){
-                answer = end - start;
-                break;
-            }else{
-                if(flag){
-                    for(int i=start+1; i<=end; i++){
-                        if(line[i] == 'H' || line[i] == 'G'){
-                            start = i;
-                            break;
-                        }
+        for(int i=0;i<=101;i++){
+            for(int j=i+1; j<=101; j++){
+                if((line[i] == 'G' || line[i] == 'H') && (line[j] == 'G' || line[j] == 'H')){
+                    int cntH = 0;
+                    int cntG = 0;
+                    
+                    for(int k=i; k<j+1; k++){
+                        if(line[k] == 'H'){cntH++;}
+                        else if(line[k] == 'G'){cntG++;}
                     }
-                }else{
-                    for(int i=end-1; i>=start; i--){
-                        if(line[i] == 'H' || line[i] == 'G'){
-                            end = i;
-                            break;
-                        }
+                    if(cntH == cntG){
+                        answer = Math.max(answer, j-i);
                     }
                 }
-                flag = !flag;
             }
         }
         System.out.println(answer);
