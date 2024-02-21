0e9a2b6c0564ba4bded99d9d881486ac62b76c23
Habin <108309664+HABINOH@users.noreply.github.com>
2024-02-21 16:52:58 +0900

    Title: 가장 가까운 두 점 사이의 거리, Time: 93ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240221/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
+++ 240221/가장 가까운 두 점 사이의 거리/closest-pair-of-points.java
@@ -5,20 +5,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+
         int[][] point = new int[n][2];
+
         for(int i=0;i<n;i++){
             StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
             point[i][0] = Integer.parseInt(stk.nextToken());
             point[i][1] = Integer.parseInt(stk.nextToken());
         }
-        int answer = Integer.MAX_VALUE;
+        int dis = Integer.MAX_VALUE;
+        int answer = 0;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                int calX = (point[i][0] - point[j][0]) * (point[i][0] - point[j][0]);
-                int calY = (point[i][1] - point[j][1]) * (point[i][1] - point[j][1]);
-                answer = Math.min(answer, calX+calY);
+                //i j 위치 차이 구하기
+                int compDis = Math.abs(point[i][0] - point[j][0]) + Math.abs(point[i][1] - point[j][1]);
+                //작은 값 갱신하기
+                if(dis > compDis){
+                    answer = Math.abs(point[i][0] - point[j][0]) * Math.abs(point[i][0] - point[j][0]);
+                    answer += Math.abs(point[i][1] - point[j][1]) * Math.abs(point[i][1] - point[j][1]);
+                    dis = compDis;
+                }
+                //갱신되면 두 점사이 거리의 제곱값 저장
             }
         }
+        //출력
         System.out.print(answer);
     }
 }
