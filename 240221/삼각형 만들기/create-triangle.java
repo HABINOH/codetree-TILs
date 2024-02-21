e435acb52380f41ff8b5d5e594a24127f822af81
Habin <108309664+HABINOH@users.noreply.github.com>
2024-02-21 17:53:52 +0900

    Title: 삼각형 만들기, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240221/삼각형 만들기/create-triangle.java
+++ 240221/삼각형 만들기/create-triangle.java
@@ -16,7 +16,7 @@ public class Main {
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 for(int k=j+1;k<n;k++){
-                    answer = Math.max(answer, Math.abs(getRectangleArea(i,j,k)));
+                    answer = Math.max(answer, getRectangleArea(i,j,k));
                 }
             }
         }
@@ -25,8 +25,8 @@ public class Main {
     public static int getRectangleArea(int one, int two, int three){
         int cal = 0;
         if(((x[one] == x[two]) || (x[one] == x[three]) || (x[two] == x[three])) || ((y[one] == y[two]) || (y[one] == y[three]) || (y[two] == y[three]))) {
-            cal = ((x[one] * y[two]) + (x[two] * y[three]) + (x[three] * y[one]))
-            - ((x[two] * y[one]) + (x[three] * y[two]) + (x[one] * y[three]));
+            cal = (Math.abs(x[one] * y[two]) + Math.abs(x[two] * y[three]) + Math.abs(x[three] * y[one]))
+            - (Math.abs(x[two] * y[one]) + Math.abs(x[three] * y[two]) + Math.abs(x[one] * y[three]));
         }
         return cal;
     }
