13fd4c1fa7b73a9a3d4224196bf330b370ab8bc8
Habin <108309664+HABINOH@users.noreply.github.com>
2024-02-21 17:51:21 +0900

    Title: 삼각형 만들기, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240221/삼각형 만들기/create-triangle.java
+++ 240221/삼각형 만들기/create-triangle.java
@@ -25,8 +25,8 @@ public class Main {
     public static int getRectangleArea(int one, int two, int three){
         int cal = 0;
         if(((x[one] == x[two]) || (x[one] == x[three]) || (x[two] == x[three])) || ((y[one] == y[two]) || (y[one] == y[three]) || (y[two] == y[three]))) {
-            cal = ((x[one] * y[two]) + (x[two] * y[three]) + (x[three] * y[one]))
-            - ((x[two] * y[one]) + (x[three] * y[two]) + (x[one] * y[three]));
+            cal = Math.abs(((x[one] * y[two]) + (x[two] * y[three]) + (x[three] * y[one]))
+            - ((x[two] * y[one]) + (x[three] * y[two]) + (x[one] * y[three])));
         }
         return cal;
     }
