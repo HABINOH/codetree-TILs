99a0fab0cf72d6db1cc34dcd79ef106ebd20d545
Habin <108309664+HABINOH@users.noreply.github.com>
2024-02-22 22:17:39 +0900

    Title: 삼각형 만들기, Time: 124ms, Memory: 9MB, Status: Passed - CodeTree

--- 240222/삼각형 만들기/create-triangle.java
+++ 240222/삼각형 만들기/create-triangle.java
@@ -7,35 +7,32 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+
         for(int i=0;i<n;i++){
             StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
             x[i] = Integer.parseInt(stk.nextToken());
             y[i] = Integer.parseInt(stk.nextToken());
         }
-        int answer = Integer.MIN_VALUE;
+        int answer = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i!=j){
                     for(int k=0;k<n;k++){
-                        if(k!=i && k!=j){
-                            answer = Math.max(answer, getRectangleArea(i,j,k));
+                        if(i!=k && j!=k){
+                            if((x[i] == x[j] || x[i] == x[k] || x[j] == x[k]) && 
+                            (y[i] == y[j] || y[i] == y[k] || y[j] == y[k])){
+                                answer = Math.max(answer, getArea(i,j,k));
+                            }
                         }
                     }
                 }
             }
         }
-        System.out.print(answer);
+        System.out.println(answer);
     }
-    public static int getRectangleArea(int one, int two, int three){
-        int width = 0, height = 0;
-        if(((x[one] == x[two]) || (x[one] == x[three]) || (x[two] == x[three])) || ((y[one] == y[two]) || (y[one] == y[three]) || (y[two] == y[three]))) {
-            // x축과 y축에 평행한 변을 찾아 넓이 계산
-            width = Math.max(Math.abs(x[one] - x[two]), Math.abs(x[one] - x[three]));
-            width = Math.max(width, Math.abs(x[two] - x[three]));
-            
-            height = Math.max(Math.abs(y[one] - y[two]), Math.abs(y[one] - y[three]));
-            height = Math.max(height, Math.abs(y[two] - y[three]));
-        }
-        return width * height; // 직사각형 삼각형의 넓이 반환
+    public static int getArea(int i, int j, int k){
+        return Math.abs( 
+            (x[i] * y[j] + x[j] * y[k] + x[k] * y[i]) - 
+            (x[j] * y[i] + x[k] * y[j] + x[i] * y[k]) );
     }
 }
