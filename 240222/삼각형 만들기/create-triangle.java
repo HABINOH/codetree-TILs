e6fb97b223a0db5af72d9c9ce0305348437c6a0e
Habin <108309664+HABINOH@users.noreply.github.com>
2024-02-22 17:57:32 +0900

    Title: 삼각형 만들기, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240222/삼각형 만들기/create-triangle.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int[] x = new int[100];
+    static int[] y = new int[100];
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            StringTokenizer stk = new StringTokenizer(br.readLine(), " ");
+            x[i] = Integer.parseInt(stk.nextToken());
+            y[i] = Integer.parseInt(stk.nextToken());
+        }
+        int answer = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j){
+                    for(int k=0;k<n;k++){
+                        if(k!=i && k!=j){
+                            answer = Math.max(answer, getRectangleArea(i,j,k));
+                        }
+                    }
+                }
+            }
+        }
+        System.out.print(answer);
+    }
+    public static int getRectangleArea(int one, int two, int three){
+        // int cal = 0;
+        // if(((x[one] == x[two]) || (x[one] == x[three]) || (x[two] == x[three])) || ((y[one] == y[two]) || (y[one] == y[three]) || (y[two] == y[three]))) {
+        //     cal = (Math.abs(x[one] * y[two]) + Math.abs(x[two] * y[three]) + Math.abs(x[three] * y[one]))
+        //     - (Math.abs(x[two] * y[one]) + Math.abs(x[three] * y[two]) + Math.abs(x[one] * y[three]));
+        // }
+        // x축과 y축에 평행한 변을 찾아 넓이 계산
+        int width = Math.max(Math.abs(x[one] - x[two]), Math.abs(x[one] - x[three]));
+        width = Math.max(width, Math.abs(x[two] - x[three]));
+        
+        int height = Math.max(Math.abs(y[one] - y[two]), Math.abs(y[one] - y[three]));
+        height = Math.max(height, Math.abs(y[two] - y[three]));
+        
+        return width * height; // 직사각형 삼각형의 넓이 반환
+        // return cal;
+    }
+}
